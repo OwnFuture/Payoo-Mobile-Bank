739d6cf87a5042016efe86828e7bfe21e45f05d6
OwnFuture <faisalmithun1316@gmail.com>
2026-02-18 14:20:12 +0600

    added js to login page and just created homepage without doing anything

--- Script/login.js
+++ Script/login.js
@@ -1,1 +1,21 @@
 console.log('Login option created');
+
+document.getElementById('login-btn').addEventListener('click',function(){
+    //get the contact number
+    const inputMobile = document.getElementById('input-mobile');
+    const mobileNumber = inputMobile.value;
+    console.log(mobileNumber);
+
+    //get the pin number
+    const inputPin = document.getElementById('input-pin');
+    const pinNumber = inputPin.value;
+    console.log(pinNumber);
+
+    //match contact number and pin
+    if(mobileNumber==='01581642874' && pinNumber==='9767'){
+        window.location.assign('/home.html');
+    }else{
+        alert('Login Failed');
+        return;
+    }
+})
