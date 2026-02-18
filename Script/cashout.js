b930e00e13eb0b78207576d49ed8a20f30a3b695
OwnFuture <faisalmithun1316@gmail.com>
2026-02-18 23:05:19 +0600

    cashout js and common function (machine.js) added

--- Script/cashout.js
+++ Script/cashout.js
@@ -1,3 +1,38 @@
 document.getElementById('cashout-btn').addEventListener('click',function(){
+    //get the agent number
+
+    const agentNumber = getValueFromInput('input-agent-number');
+    if(agentNumber.length!==11){
+        alert('Invalid Agent Number');
+        return;
+    }
+
+    //get the cashout amount
+
+    const cashoutAmount = getValueFromInput('input-cashout-amount');
     
+
+    //get the current balance
+
+    const currentBalance = document.getElementById('balance');
+    const Balance = currentBalance.innerText;
+    
+    //calculate new balance
+
+    const newBalance = Number(Balance) - cashoutAmount;
+    if(newBalance<0){
+        alert("Invalid amount");
+        return;
+    }
+
+    //get the pin
+
+    const cashoutPin = getValueFromInput('input-cashout-pin');
+    
+    if(cashoutPin === '9767'){
+        alert('Cashout Succesfull');
+        currentBalance.innerText = newBalance;
+    }else{
+        alert('Invalid PIN');
+    }
 })
