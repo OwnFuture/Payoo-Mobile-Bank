018f84cf42d636156dc44a3fc5b1c06a8f8cd853
OwnFuture <faisalmithun1316@gmail.com>
2026-02-20 21:19:34 +0600

    Done

--- Script/machine.js
+++ Script/machine.js
@@ -3,3 +3,16 @@ function getValueFromInput(id){
     const value = input.value;
     return value;
 }
+
+//machine id> hide id > show id
+
+function showOnly(id){
+    const addMoney = document.getElementById('add-money');
+    const cashOut = document.getElementById('cashout');
+
+    addMoney.classList.add('hidden');
+    cashOut.classList.add('hidden');
+
+    const selected = document.getElementById(id);
+    selected.classList.remove('hidden');
+}
